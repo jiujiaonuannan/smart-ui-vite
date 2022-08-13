10eaf247632b18a377b923dcd27139fe2cc33ba2
alvin.wang <alvin.wang@binance.com>
2022-08-13 15:14:18 +0200

    chore: commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,26 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 
+const rollupOptions = {
+  external: ["vue", "vue-router"],
+  output: {
+    globals: {
+      vue: "Vue",
+    },
+  },
+};
+
 export default defineConfig({
   plugins: [vue(), vueJsx({})],
+  build: {
+    rollupOptions,
+    minify: false,
+    lib: {
+      entry: "./src/entry.ts",
+      name: "SmartUI",
+      fileName: "smart-ui",
+      // 导出模块格式
+      formats: ["esm", "umd", "iife"],
+    },
+  },
 });
